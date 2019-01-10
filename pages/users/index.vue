d43868e58169fcc4cef81bcfd578c4b66db4db38
lemkyj <kmwn0024@humbermail.ca>
2019-01-10 17:24:03 -0500

    save

---
+++ pages/users/index.vue
@@ -0,0 +1,25 @@
+<template> 
+    <div class="container">
+        <div class="form-group">
+          <input type="text"
+            class="form-control" aria-describedby="helpId" placeholder=""
+            v-model="userId">
+           <button class="btn btn-outline-dark btn-block">
+                Load User
+           </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        onLoadUser() 
+    }
+}
+</script>
